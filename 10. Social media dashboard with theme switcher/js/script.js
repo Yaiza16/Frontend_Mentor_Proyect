80128d9f2d3162319ebdd3029a48e33d145d6950
Yaiza16 <yaizavallejo16@gmail.com>
2021-04-10 12:22:01 +0200

    JS hecho y pequeñas modificaciones en CSS

---
+++ 10. Social media dashboard with theme switcher/js/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('#button');
+const body = document.body;
+
+
+button.addEventListener('click', () =>{
+    body.classList.toggle('dark')
+    console.log(body.className)
+
+})
